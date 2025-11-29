f3d4ef3c5e0fe05d8cd032a46e531e7d53839b12
xiaoyulyu999 <info@bloomora.net>
2025-11-29 13:52:49 +0000

    home

---
+++ linkedListCycle.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Xiaoyu Lyu on 29/11/2025.
+//
+
+#ifndef LEETCODE_LINKEDLISTCYCLE_HPP
+#define LEETCODE_LINKEDLISTCYCLE_HPP
+#include <vector>
+
+#include "ListNode.h"
+
+
+class linkedListCycle {
+
+public:
+    static bool isCycle(ListNode* head);
+};
+
+
+#endif //LEETCODE_LINKEDLISTCYCLE_HPP
